35a14d5c355fe4e4b05a19975d51832d932503c7
John Prince <jtprince@gmail.com>
2013-05-08 14:04:35 -0600

    progress

--- lib/dotmation.rb
+++ lib/dotmation.rb
@@ -1,15 +1,17 @@
 require 'open-uri'
+require 'fileutils'
 
 class Dotmation
 
   CONFIG_HOME = ENV['XDG_CONFIG_HOME'] || "#{ENV['HOME']}/.config"
   DEFAULT_CONFIG_PATH = CONFIG_HOME + '/dotmation/config'
-  GITHUB_PATH = "config/dotmation/config"
+  DEFAULT_CONFIG_GITHUB_PATH = "config/dotmation/config"
+  GITHUB_REPO_BASE = "https://github.com"
 
   attr_accessor :config_data
 
   attr_accessor :data
-  attr_accessor :links
+  attr_accessor :repos
 
   # quick check to see if this looks like a dotmation config file
   def is_config?
@@ -21,52 +23,105 @@ class Dotmation
   def initialize(arg=DEFAULT_CONFIG_PATH)
     @config_data = 
       if File.exist?(arg)
+        @config_filename = arg
         IO.read(arg)
       else # github name
-        uri = "https://raw.github.com/#{arg}/#{GITHUB_PATH}"
+        uri = "https://raw.github.com/#{arg}/#{DEFAULT_CONFIG_GITHUB_PATH}"
         open(uri) {|io| io.read }
       end
     warn "couldn't find config file locally or on github" unless is_config?
     read_config!
   end
 
-  # returns the github repos from the links
-  def repos_from_links(links)
-    abort 'IMPLEMENT!'
+  def repo_cache
+    @repo_cache ||= File.expand_path(@data[:repo_cache])
+  end
+
+  def update
+    update_github_repos!
+  end
+
+  def update_github_repos!
+    FileUtils.mkpath(repo_cache) unless File.directory?(repo_cache)
+    Dir.chdir(repo_cache) do
+      repos_grouped_by_name(:github).each do |user_repo, repos|
+        (user, repo) = user_repo.split('/')
+        FileUtils.mkdir(user) unless File.directory?(user)
+        Dir.chdir(user) do
+          if File.directory?(repo)
+            Dir.chdir(repo) { print `git pull` }
+          else
+            print `git clone #{GITHUB_REPO_BASE}/#{user}/#{repo}.git`
+          end
+        end
+      end
+    end
+  end
+
+  def repos_grouped_by_name(type=:github)
+    repos.select {|r| r.type == type }.group_by {|r| r.repo_name }
   end
 
   def read_config!
-    (@data, @links) = ConfigReader.new.read(@config_data)
+    (@data, @repos) = ConfigReader.new.read(@config_data, @config_filename)
+  end
+
+  module Repo
+    attr_accessor :path
+    attr_accessor :links
+
+    def initialize(path)
+      @path = path
+      @links = []
+    end
+
+    def type
+      self.class.to_s.downcase.split('::').last.to_sym
+    end
+
+
+    def self.classes_as_lc_symbols
+      self.constants.map {|v| v.to_s.downcase.to_sym }
+    end
+
+    class Local
+      include Repo
+    end
+
+    class Github
+      include Repo
+
+      def repo_name
+        path[%r{[^/]+/[^/]+}]
+      end
+    end
+
   end
 
   # type is :github or :local
-  Link = Struct.new(:type, :methd, :file, :linkname)
-
+  Link = Struct.new(:methd, :file, :linkname)
   class Link
-    TYPES = [:local, :github]
     METHODS = [:dot, :cfg, :ln]
   end
 
   class ConfigReader
     # returns a list of links and a hash of any other values given outside
     # standard blocks
-    def read(config_data)
+    def read(config_data, config_filename=nil)
+      @available_repos = Repo.classes_as_lc_symbols
       @data = {}
-      @links = []
-      eval config_data
-      [@data, @links]
+      @repos = []
+      eval config_data, binding, config_filename
+      [@data, @repos]
     end
 
     def method_missing(methd, *argv, &block)
-      p methd
-      methd = methd.to_sym
       if Link::METHODS.include?(methd)
-        @links << Link.new(@last_type, methd, *argv)
-      elsif Link::TYPES.include?(methd) && block
-        @last_type = methd
+        @repos.last.links << Link.new(methd, *argv)
+      elsif @available_repos.include?(methd) && block
+        @repos << Repo.const_get(methd.to_s.capitalize).new( *argv )
         instance_eval(&block)
       else
-        puts "NO METHOD putting in hash: #{methd}"
         arg = (argv.size==1 ? argv.first : argv)
         @data[methd] = arg
       end
--- spec/dotmation_spec.rb
+++ spec/dotmation_spec.rb
@@ -12,20 +12,40 @@ describe Dotmation do
       dotmation = Dotmation.new(@config)
     end
 
-    describe 'actions' do
+    describe 'the data' do
 
-      before do
-        @dotmation = Dotmation.new(@config)
+      let(:dotmation) { Dotmation.new(@config) }
+
+      it 'knows data' do
+        data = dotmation.data
+        data.should be_an(Hash)
+        data[:repo_cache].should == "spec/testfiles/tmprepos"
       end
 
-      it 'creates lots of link objects' do
-        @dotmation.links.should be_an(Array)
+      it 'has repos with links' do
+        repos = dotmation.repos
+        repos.size.should == 4
+        repos.map(&:type).partition.to_a.should == [:github, :github, :local, :github]
       end
 
-      it 'knows about repos' do
-        @dotmation.data.should be_an(Hash)
+      it 'knows which git repos it needs to get' do
+        hash = dotmation.repos_grouped_by_name
       end
 
+    end
+
+    describe 'an update' do
+      let(:dotmation) { Dotmation.new(@config) }
+
+      it 'downloads github repos' do
+        dotmation.update
+        ["jtprince/dotfiles", "robbyrussell/oh-my-zsh"].each do |path|
+          base = TESTFILES + "/tmprepos/" + path
+          git_file = base + "/.git"
+          File.exist?( git_file ).should be_true
+          FileUtils.rm_rf(base)
+        end
+      end
     end
 
   end
